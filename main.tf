b66a482dcd5dd7e6d3380c67ee53c32930279840
Jerome Wirsztel <jeromewir@users.noreply.github.com>
2025-08-11 18:04:31 +0200

    Updated email

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ variable "project_id" {
 resource "google_project_iam_member" "mysaas_binding_bq" {
   project = var.project_id
   role    = "roles/bigquery.dataViewer"
-  member  = "serviceAccount:customer-accessor@my-saas-project.iam.gserviceaccount.com"
+  member  = "serviceAccount:test-service-account-jerome@coalition-mfa-mvp-395313.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "mysaas_binding_storage" {
   project = var.project_id
   role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:customer-accessor@my-saas-project.iam.gserviceaccount.com"
+  member  = "serviceAccount:test-service-account-jerome@coalition-mfa-mvp-395313.iam.gserviceaccount.com"
 }
